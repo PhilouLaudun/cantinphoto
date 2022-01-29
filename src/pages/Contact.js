cd244c9be1a9d85507786bdaeccdcf9e615492be
PhilouLaudun <77538675+PhilouLaudun@users.noreply.github.com>
2022-01-29 11:26:26 +0100

    Developpement page contact - debut présentation

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -5,13 +5,33 @@ import Navigation from '../componants/Navigation';
 const Contact = () => {
   return (
     <main>
-      <Navigation />
-      <Logo />
-    <div>
-      <h1> Page Contact</h1>
+      <div className="contact">
+        <Navigation />
+        <Logo />
+        <div className="contact-infos">
+          <div className="address">
+            <div className="content">
+              <h4>Adresse</h4>
+              <p>12 rue du code </p>
+              <p>34000 MONTPELLIER</p>
+            </div>
+          </div>
+          <div className="phone">
+            <div className="content">
+              <h4>Téléphone </h4>
+              <p>04 67 66 66 66 </p>
+            </div>
+          </div>
+          <div className="email">
+            <div className="content">
+              <h4>Email </h4>
+              <p>charles.cantin@orange.fr</p>
+            </div>
+          </div>
+        </div>
       </div>
     </main>
-      );
+  );
 };
 
 export default Contact;
